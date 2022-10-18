54a8926a4eaac381265b475b2c31799d7813e870
Dalia Jaber <daliajaber96@gmail.com>
2022-10-18 17:24:21 +0300

    Complete Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('#create-task-form');
+    form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    buildElement(e.target.new_task_description.value);
+    form.reset();
+     
+  })
+
 });
+function buildElement(inputValue){
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click',deleteButton)
+  btn.textContent = " X ";
+  li.textContent = `${inputValue} `;
+  console.log(li);
+  li.appendChild(btn);
+  document.querySelector('#tasks').append(li);
+  
+  
+}
+function deleteButton(e){
+  e.target.parentNode.remove();
+
+
+}
